151e3412c51efdb2085c54bcb717a865327ea6ba
Ante <antedr4gun@gmail.com>
2025-07-28 13:25:51 +0200

    style sidebar

--- frontend/vite-project/src/App.tsx
+++ frontend/vite-project/src/App.tsx
@@ -1,16 +1,35 @@
-import { useState } from "react";
+import { use, useState } from "react";
 import "./App.css";
 import { PrimeReactProvider } from "primereact/api";
 import { Button } from "primereact/button";
 import { InputText } from "primereact/inputtext";
+import { Chart } from "primereact/chart";
 
 export default function App() {
   const [value, setValue] = useState<string>("");
+
   return (
     <PrimeReactProvider>
       <div className="app">
         <div className="sidebar">
-          <Button label="New Chat" icon="pi pi-plus" size="small" />
+          <Button
+            label="Novi upit"
+            icon="pi pi-plus"
+            size="small"
+            className="no-style-button"
+          />
+
+          <div className="history">
+            <h3>
+              <div
+                className="pi pi-history
+"
+              ></div>{" "}
+              Povijest
+            </h3>
+            <Button label="Upit 2" size="small" className="no-style-button" />
+            <Button label="Upit 1" size="small" className="no-style-button" />
+          </div>
         </div>
         <div className="container">
           <div>
@@ -19,10 +38,7 @@ export default function App() {
               placeholder="Postavi pitanje"
               onChange={(e) => setValue(e.target.value)}
             />
-            <Button
-              icon="pi pi-send
-"
-            />
+            <Button icon="pi pi-send" />
           </div>
         </div>
       </div>
