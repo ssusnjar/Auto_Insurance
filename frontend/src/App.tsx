014ced75e9bbf21c59654c62561f7fc65d384ca8
Anastazija Celar <anastazija.celar@fer.unizg.hr>
2025-07-29 11:29:45 +0200

    updated buttons and chart

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./App.css";
 import { PrimeReactProvider } from "primereact/api";
 import { Button } from "primereact/button";
 import { InputText } from "primereact/inputtext";
 import { Chart } from "primereact/chart";
+import { Divider } from "primereact/divider";
+import { Card } from "primereact/card";
 
 export default function App() {
   const [value, setValue] = useState<string>("");
@@ -40,27 +42,30 @@ export default function App() {
     },
   };
 
+  const startNewChat = () => {
+    setValue("");
+    setLoading(false);
+    setChartData("");
+  }
+
   return (
     <PrimeReactProvider>
       <div className="app">
         <div className="sidebar">
-          <Button
-            label="Novi upit"
-            icon="pi pi-plus"
-            size="small"
-            className="no-style-button"
-          />
-
-          <div className="history">
-            <h3>
-              <div
-                className="pi pi-history
-"
-              ></div>{" "}
-              Povijest
-            </h3>
-            <Button label="Upit 2" size="small" className="no-style-button" />
-            <Button label="Upit 1" size="small" className="no-style-button" />
+          <div className="chat-section">  
+            <Button
+              label="Novi upit"
+              icon="pi pi-plus"
+              size="small"
+              className="p-button-sm p-button-text w-full mb-2"
+              onClick={startNewChat} 
+            />
+            <Divider />
+            <div className="history">
+                <h4><i className="pi pi-history" />Povijest</h4>
+                <Button label="Upit 2" className="p-button-text p-button-sm w-full" />
+                <Button label="Upit 1" className="p-button-text p-button-sm w-full" />
+            </div>
           </div>
         </div>
         <div className="container">
@@ -70,7 +75,9 @@ export default function App() {
             ) : (
               <>
                 {chartData?.datasets && chartData.datasets.length > 0 && (
+                  <Card className="chart-box">
                   <Chart type="pie" data={chartData} options={chartOptions} />
+                  </Card>
                 )}
               </>
             )}
@@ -80,6 +87,7 @@ export default function App() {
                 value={value}
                 placeholder="Postavi pitanje"
                 onChange={(e) => setValue(e.target.value)}
+                className="w-20rem"
               />
               <Button icon="pi pi-send" onClick={handleClick} />
             </div>
